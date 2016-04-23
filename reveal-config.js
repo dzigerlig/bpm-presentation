44da672c04f61ae84349567f44d72051fcb31e7b
Daniel Zigerlig <dzigerli@hsr.ch>
2016-04-23 11:15:07 +0200

    added notes

--- reveal-config.js
+++ reveal-config.js
@@ -1,3 +1,9 @@
 Reveal.initialize({
-    history: true
+    history: true,
+    dependencies: [
+        { src: 'node_modules/reveal.js/plugin/markdown/marked.js' },
+        { src: 'node_modules/reveal.js/plugin/markdown/markdown.js' },
+        { src: 'node_modules/reveal.js/plugin/notes/notes.js', async: true },
+        { src: 'node_modules/reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
+    ]
 });
